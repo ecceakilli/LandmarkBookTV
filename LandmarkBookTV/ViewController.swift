233e4fcc2d8ecd5acd282d982f88919d553a8714
eceImac <ece.akilli@icisleri.gov.tr>
2021-12-20 16:07:30 +0300

    Initial Commit

---
+++ LandmarkBookTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkBookTV
+//
+//  Created by ece on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
